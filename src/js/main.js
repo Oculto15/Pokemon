5bf9dd24d33d44748434236c1d480a7f951a1868
Joshua Dean <dea16006@byui.edu>
2022-12-09 15:12:22 -0700

    Added search functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,16 @@ import {
   loadHeaderFooter
 } from './utils.js';
 
+
 loadHeaderFooter();
+
+function searchPokemon(){
+  const searchValue = document.querySelector('#searchName').value;
+
+  window.location.href = `http://localhost:5173/pokedetails/index.html?pokemon=${searchValue}`; 
+
+}
+
+document.querySelector('#btnSearch').addEventListener('click', searchPokemon);
 
 
